d50adc488cca5a0813bed0feae79b8ebf913d812
Yuri Mataev <yuri@mataev.com>
2019-10-07 02:26:05 -0400

    Added multiple wallet generation capability

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
+newripple(10);
 
-var qrcodeAddress = new QRCode(document.getElementById("qrcodeAddress"),{width: 120,height: 120});
-var qrcodeSecret = new QRCode(document.getElementById("qrcodeSecret"),{width: 120, height: 120});
+function newripple(count) {
+	var api = new ripple.RippleAPI();
+	
+	for (var i=1; i<=count; i++) {
+		var wallet = 'wallet'+i;
+		if (document.getElementById(wallet) === null) {
+			var clone = document.getElementById('wallet1').cloneNode(true);
+			clone.setAttribute( 'id', wallet);
+			document.querySelector('#container').appendChild( clone );
+		}
+		wallet = '#' + wallet;
+		var account = api.generateAddress();
+		document.querySelector(wallet + " .qr-address").innerHTML = '';
+		var qrcodeAddress = new QRCode(document.querySelector(wallet + " .qr-address"),{width: 90,height: 90});
+		document.querySelector(wallet + " .qr-secret").innerHTML = '';
+		var qrcodeSecret = new QRCode(document.querySelector(wallet + " .qr-secret"),{width: 90, height: 90});
 
-newripple();
-
-function newripple() {
-  var api = new ripple.RippleAPI();
-  var account = api.generateAddress();
-  document.getElementById("address").innerHTML = account.address;
-  document.getElementById("secret").innerHTML = account.secret;
-  qrcodeAddress.makeCode(account.address);
-  qrcodeSecret.makeCode(account.secret);
+		document.querySelector(wallet + " .address").innerHTML = account.address;
+		document.querySelector(wallet + " .secret").innerHTML = account.secret;
+		qrcodeAddress.makeCode(account.address);
+		qrcodeSecret.makeCode(account.secret);
+	}
 }
