43b88fcd7b6ce15938331602422d564d3c2e9ae7
Yuri Mataev <yuri@mataev.com>
2019-11-04 02:42:41 -0500

    Added ETH wallet generation to make this a universal paper wallet

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
-newripple(10);
+generate();
 
-function newripple(count) {
-	var api = new ripple.RippleAPI();
+function generate() {
+  var walletCount = parseInt(document.getElementById('wallet_count').value);
+  var c = document.getElementById('currency');
+  var currency = c.options[c.selectedIndex].value;
 
-  // destroy all but the first wallet
+  var pageTitle = currency + ' Paper Wallet';
+  document.getElementById('heading').innerHTML = document.title = pageTitle;
+
+  if (walletCount <= 0) {
+    alert ('Enter an integer greater than 0 for wallet count.');
+    return;
+  }
+
+  switch (currency) {
+    case 'XRP':
+      genWallets (walletCount, function () {
+        var api = new ripple.RippleAPI();
+        var account = api.generateAddress();
+        return {
+          'address' : account.address,
+          'secret' : account.secret
+        }
+      });
+      break;
+    case 'ETH':
+      genWallets (walletCount, function () {
+        var api = new Web3EthAccounts();
+        var account = api.create();
+        return {
+          'address' : account.address,
+          'secret' : account.privateKey
+        }
+      });
+      break;
+  }
+}
+
+function genWallets(count, addressGenerator) {
+	// destroy all but the first wallet
   var nl = document.querySelectorAll('div.wallet');
   nl.forEach(
     function(currentValue, currentIndex, listObj) {
@@ -21,15 +56,15 @@ function newripple(count) {
 			document.querySelector('#container').appendChild( clone );
 		}
 		wallet = '#' + wallet;
-		var account = api.generateAddress();
-		document.querySelector(wallet + " .qr-address").innerHTML = '';
-		var qrcodeAddress = new QRCode(document.querySelector(wallet + " .qr-address"),{width: 90,height: 90});
-		document.querySelector(wallet + " .qr-secret").innerHTML = '';
-		var qrcodeSecret = new QRCode(document.querySelector(wallet + " .qr-secret"),{width: 90, height: 90});
+		var account = addressGenerator();
+		document.querySelector(wallet + ' .qr-address').innerHTML = '';
+		var qrcodeAddress = new QRCode(document.querySelector(wallet + ' .qr-address'),{width: 90,height: 90});
+		document.querySelector(wallet + ' .qr-secret').innerHTML = '';
+		var qrcodeSecret = new QRCode(document.querySelector(wallet + ' .qr-secret'),{width: 90, height: 90});
 
-		document.querySelector(wallet + " .address").innerHTML = account.address;
-		document.querySelector(wallet + " .secret").innerHTML = account.secret;
-		qrcodeAddress.makeCode(account.address);
-		qrcodeSecret.makeCode(account.secret);
+		document.querySelector(wallet + ' .address').innerHTML = account['address'];
+		document.querySelector(wallet + ' .secret').innerHTML = account['secret'];
+		qrcodeAddress.makeCode(account['address']);
+		qrcodeSecret.makeCode(account['secret']);
 	}
 }
